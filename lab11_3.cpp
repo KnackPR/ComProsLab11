1b81bd91771bb10a1665669a4fbcfbaeae229af5
kack <peerawich_r@cmu.ac.th>
2023-01-03 15:55:32 +0700

    ทำไม strodใช้ไม่ได้อ่ะ

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    ifstream source("score.txt");
+    string textline ;
+    int count = 0 ;
+    double mean = 0 , stdev = 0;
+    double sum = 0 , sum2 = 0 ;
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()),2);
+        count++ ; 
+    } 
+    mean = sum/count ;
+    stdev = sqrt((sum2/count) - pow(mean,2)) ;
+    cout << "Number of data = " << count << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n';
+    cout << "Standard deviation = " << stdev << '\n';
+}
